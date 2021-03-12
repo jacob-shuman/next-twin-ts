b1a6c1421014c511a6b65a738377107e5afccf17
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2021-03-11 22:33:49 -0500

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import tw from "twin.macro";
+
+export default function Home() {
+  return (
+    <>
+      <h1 css={tw`p-4 text-4xl font-bold`}>
+        Next.js + Netlify + twin.macro + Typescript
+      </h1>
+    </>
+  );
+}
